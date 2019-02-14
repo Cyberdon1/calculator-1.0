ace61fb11ff95b2a129ba7b08d99a8ae6ed3b085
donavan stokes and Jen Bruce <donavan@>
2019-02-13 20:49:00 -0800

    Add addition js, debug addition segment, adjust html tags to match.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,70 @@
 $(document).ready(function() {
-  $("addnumber form").submit(function(event) {
-    event.preventDefault();
-    var add = function(number1add, number2add) {
-      return number1add + number2add;
-    };
 
-    var number1add= parseInt(prompt("Enter a number:"));
-    var number2add = parseInt(prompt("Enter another number:"));
-    var resultadd = add(number1add, number2add);
-    alert(resultadd);
-  };
+  // $("#addnumber form").submit(function(event) {
+  //   var number1addInput = parseInt($("input#number1add").val());
+  //   var number2addInput = parseInt($("input#number2add").val());
+  //
+  //
+  //   var add = function(number1add, number2add) {
+  //     //console.log("first number: " + number1add + " " + "Second Number: " + number2add);
+  //     return number1add + number2add;
+  //   };
+  //
+  //   var resultAdd = add(number1addInput, number2addInput);
+  //
+  //   $(".resultadd").text(resultAdd + " !");
+  //
+  //   $("additionresult").show();
+  //
+  //   event.preventDefault();
+  // });
 
   $("subtractnumber form").submit(function(event) {
-    event.preventDefault();
+    console.log ("did load")
+    var number1diffInput = parseInt($("input#number1diff").val());
+    var number2diffInput = parseInt($("input#number2diff").val());
 
     var subtract = function(number1diff, number2diff) {
+      console.log("first number: " + number1diff + " " + "second number: " + number2diff);
       return number1diff - number2diff;
     };
 
-    var number1diff = parseInt(prompt("Enter a number:"));
-    var number2diff = parseInt(prompt("Enter another number:"));
-    var resultdiff = subtract(number1diff, number2diff;
-    alert(resultdiff);
-  };
-  $("multiplynumber form").submit(function(event) {
+
+    var resultdiff = subtract(number1diff, number2diff);
+
+    $(".subtractionresult").text(resultdiff + "!");
+
+    $(".subtractionresult").show();
+
+
     event.preventDefault();
+  });
 
-    var multiply = function(number1prod, number2prod) {
-      return number1prod * number2prod;
-    };
-
-    var number1prod = parseInt(prompt("Enter a number:"));
-    var number2prod = parseInt(prompt("Enter another number:"));
-    var resultprod = multiply(number1prod, number2prod);
-    alert(resultprod);
-  };
-
-  $("dividenumber form").submit(function(event) {
-    event.preventDefault();
-
-    var divide = function(number1quot, number2quot) {
-      return number1quot / number2quot;
-    };
-
-    var number1quot = parseInt(prompt("Enter a number:"));
-    var number2quot = parseInt(prompt("Enter another number:"));
-    var resultquot = divide(number1quot, number2quot);
-    alert(resultquot);
-  };
+  // $("multiplynumber form").submit(function(event) {
+  //   event.preventDefault();
+  //
+  //   var multiply = function(number1prod, number2prod) {
+  //     return number1prod * number2prod;
+  //   };
+  //
+  //   var number1prod = parseInt(prompt("Enter a number:"));
+  //   var number2prod = parseInt(prompt("Enter another number:"));
+  //   var resultprod = multiply(number1prod, number2prod);
+  //   alert(resultprod);
+  // };
+  //
+  // $("dividenumber form").submit(function(event) {
+  //   event.preventDefault();
+  //
+  //   var divide = function(number1quot, number2quot) {
+  //     return number1quot / number2quot;
+  //   };
+  //
+  //   var number1quot = parseInt(prompt("Enter a number:"));
+  //   var number2quot = parseInt(prompt("Enter another number:"));
+  //   var resultquot = divide(number1quot, number2quot);
+  //   alert(resultquot);
+  // };
 
 
 
